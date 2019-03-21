c8faef0c7ee83955b86b0856c8bd135c5a973be5
daveed <david.alphen@gmail.com>
2019-03-20 17:32:28 -0700

    improving code

--- ruby/codesignal/grouping_dishes.rb
+++ ruby/codesignal/grouping_dishes.rb
@@ -1,25 +1,10 @@
 def groupingDishes(dishes)
-  # group by ingredient
-  grouped_hash = {}
+  grouped_hash = Hash.new { |h, k| h[k] = Array.new }
   dishes.each.with_index do |dish, index|
     dish_name = dishes[index].shift
-    dishes[index].each do |ingredient|
-      grouped_hash[ingredient] = [] unless grouped_hash[ingredient]
-      grouped_hash[ingredient] << dish_name
-    end
+    dishes[index].map { |ingredient| grouped_hash[ingredient] << dish_name }
   end
 
-  # remove ingredient with one dish
-  grouped_hash.each do |k, _|
-    grouped_hash.delete(k) if grouped_hash[k].size == 1
-  end
-
-  # transform hash to array of array
-  grouped = []
-  grouped_hash.each do |k, v|
-    grouped << [k, v.flatten.sort].flatten
-  end
-
-  # sort by first element lexicographically
-  grouped.sort
+  grouped_hash.map { |k, _| grouped_hash.delete(k) if grouped_hash[k].size == 1 }
+  grouped_hash.map { |k, v| [k, v.flatten.sort].flatten }.sort
 end
